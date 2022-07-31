0c8ff06971f7992dc58405d13a8a63a2bedc6fea
wanglifa <wanglifa1995@gmail.com>
2022-07-31 13:52:54 +0800

    feat: replace

---
+++ replace/template.ts
@@ -0,0 +1,12 @@
+
+const Replace = (str: string, from: string, to: string) => {
+  const index = str.indexOf(from)
+  if (index > -1) {
+    if (from === '') {
+      return str
+    } else {
+      return str.substring(0, index) + to + str.substring(index + from.length)
+    }
+  }
+}
+type Replace<S extends string, From extends string, To extends string> = S extends `${infer Start}${From}${infer End}` ? From extends '' ? S : `${Start}${To}${End}` : S
